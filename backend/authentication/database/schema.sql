66279defabfcb3a0459b29f86e62af42eb51117f
Peter Hilbert <pehilbert03@gmail.com>
2024-11-27 16:11:15 -0700

    Remove test entries from schema script

--- backend/authentication/database/schema.sql
+++ backend/authentication/database/schema.sql
@@ -9,9 +9,3 @@ CREATE TABLE IF NOT EXISTS users (
     pass VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-INSERT INTO users (username, email, pass)
-VALUES
-('peter', 'peter@gmail.com', 'password'),
-('peter2', 'peter2@yahoo.com', '12345'),
-('therealpeter', 'realpeter@gmail.com', 'dbacks')
