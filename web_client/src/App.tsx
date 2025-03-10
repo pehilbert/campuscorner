c0833ebae0c152eaa48f7085249b51ce3494a418
pehilbert <148654775+pehilbert@users.noreply.github.com>
2025-03-09 20:52:31 -0700

    Redo frontend in typescript

---
+++ web_client/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>CampusCorner</h1>
+    </>
+  )
+}
+
+export default App
--- web_client/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-    return (
-        <>
-            <h1>CampusCorner</h1>
-        </>
-    )
-}
-
-export default Home
